e5a021e5f08c9e043962ac1c8df587f6cea101df
Emilia <emispalenza@gmail.com>
2019-06-23 22:02:26 -0300

    Agregue CuentaUsuario

---
+++ CuentaUsuario.java
@@ -0,0 +1,7 @@
+public class CuentaUsuario {
+  public static void main (String [] args){
+    System.out.println("Esta cuenta es de pedro");
+    System.out.println("Saldo de la cuente: 200.203");
+  }
+
+}
